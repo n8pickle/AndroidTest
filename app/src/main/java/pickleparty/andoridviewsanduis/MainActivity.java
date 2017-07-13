c75a1e5b2ff81433270372caea5faaf5b738b951
n8pickle <n8pickle@gmail.com>
2017-07-13 07:22:39 -0600

    added all of the methodsof the Android Lifecycle, along with logs so they could be tracked

--- app/src/main/java/pickleparty/andoridviewsanduis/MainActivity.java
+++ app/src/main/java/pickleparty/andoridviewsanduis/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -13,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Camera camera = new Camera();
         super.onCreate(savedInstanceState);
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onCreate");
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button_id);
         final Switch switch1 = (Switch) findViewById(R.id.switch1);
@@ -27,6 +28,46 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onRestart");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(getApplicationContext().getResources().getString(R.string.tag_string), "onDestroy");
 
     }
 }
